d2ae90399408af429359b92a8dd18b0ea9cd5815
Harshraj Deshmukh <110093215+harshrajd07@users.noreply.github.com>
2024-09-22 11:27:36 +0530

    Delete 08_july directory

--- 08_july/qno.4.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include<ctype.h>
-    int main() {
-      char ch;
-      printf("Enter a lowercase character:");
-      scanf("%c",&ch);
-      printf("Uppercase equivalent: %c",toupper(ch));
-      return 0;
-    }
--- 08_july/qno3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-    int main() {
-      float c,f;
-      printf("Enter temperature in Fahrenheit: ");
-      scanf("%f",&f);
-      c= (f - 32) * (5.0 / 9.0);
-      printf("%f Fahrenheit is %f Celsius. ",f,c);
-      return 0;
-    }
