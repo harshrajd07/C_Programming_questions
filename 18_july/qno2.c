49d57acc47386726872264965de25a006c05ee11
Harshraj Deshmukh <110093215+harshrajd07@users.noreply.github.com>
2024-09-22 11:29:51 +0530

    Delete 18_july directory

--- 18_july/qno2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int unit;
-    printf("Enter the number of units consumed: \n");
-    scanf("%d",&unit);
-    (unit<=100) ? printf("The rate per unit is Rs. 3.00 and the bill is %.2f . \n",unit*3.00) : printf("The rate per unit is Rs. 4.50 and the bill is %.2f .\n",(100*3)+((unit-100)*4.50));
-
-    return 0;
-}
