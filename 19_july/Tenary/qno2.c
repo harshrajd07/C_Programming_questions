1914f07551aa63f485eed18d6803e5c1b9666c98
Harshraj Deshmukh <110093215+harshrajd07@users.noreply.github.com>
2024-09-22 11:30:13 +0530

    Delete 19_july directory

--- 19_july/Tenary/qno2.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    int n;
-    printf("Enter a number. \n");
-    scanf("%d",&n);
-    n % 2 == 0 ? printf("The number %d is even.",n) : printf("The number %d is odd.",n);
-  
-  return 0;
-}
